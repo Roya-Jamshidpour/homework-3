67a8a75bb71c4e300fac2b6a5a04096757243f11
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-04-19 17:06:15 -0500

    added quote marks to variables list

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,17 @@ let button = document.getElementById("#generate") .addEventListener("click" , ge
 
 
 // variables in characters
+var specialCharacters = [
+  '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_'];
 
-var variablesAll = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$"
+
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var variablesAll = 
 var variablesNoLowercase = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@-#$"
 var variablesNoUppercase = "0123456789abcdefghijklmnopqrstuvwxyz~!@-#$"
 var variablesNoNumbers = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$"
