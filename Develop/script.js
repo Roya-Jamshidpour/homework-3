1012bcbd3cac92bf6da9ce83cf57d7fcfe381847
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-04-19 16:57:51 -0500

    added special character variables differently

--- Develop/script.js
+++ Develop/script.js
@@ -2,94 +2,14 @@
 //generate button and event listener for when clicked//
 let button = document.getElementById("#generate") .addEventListener("click" , generatePassword);
 
-// user inputs
-let yes = "Yes"
-let no = "No"
 
 // variables in characters
-var specialCharacters =[
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowerCase = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-var upperCase = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
 
+var variablesAll = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$"
+var variablesNoLowercase = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@-#$"
+var variablesNoUppercase = "0123456789abcdefghijklmnopqrstuvwxyz~!@-#$"
+var variablesNoNumbers = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$"
+var variablesNoSpecialCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -98,24 +18,24 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// choose if you would like to include lowercase letters?
 
-function generatePassword(length, passwordLowercase, passwordUppercase, passwordNumbers, passwordSpecialCharacters){
-  // window prompt for yes or no
-  window.prompt("Would you like lowercase letters? Type \"Yes\" or \"No\"")
+function generatePassword(length, lowercase, uppercase, numbers, specialCharacters){
   
-  // pick if user wants lowercase //
-
+  // array for lowercase lowercase //
+  const randomLowerCase = lowerCase[Math.floor(Math.random() * lowerCase.length)]
+  
+  
+  // choose if you would like to include lowercase letters?
+   // window prompt for yes or no
+  window.prompt("Would you like lowercase letters? Type \"Yes\" or \"No\"")
   // function to choose random lowercase letter
-function randomLowercase(){
-  let index = Math.floor(Math.random() * "lowerCase");
-   return arr[index];
+function passwordLowercase(){
+  if(userInput === "Yes"); 
+   return randomLowerCase; 
+  }  else {
+   return(???????)
 }
-  function passwordLowercase() {
-    if (userInput === 'Yes') {
-    return console.log(randomLowercase); // if true, moves to next prompt
-  }
-}
+
 // choose if you would like to include uppercase letters?
 
 // choose if you would like to include numbers?
@@ -135,8 +55,8 @@ function generatePassword(length, passwordLowercase, passwordUppercase, password
     function passwordLength() {
       if (length < 8 || length > 128) 
         return userInput; // if true, displays users input
-       else {
-        return 'Error!' //Displays error message if user inputs anything other than the three parameters listed above
+      } else {
+        return
       
       }
     }
