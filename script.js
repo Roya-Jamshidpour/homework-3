4eca9e846ce1ee664793c789efa85c9975322671
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-04-20 18:18:33 -0500

    button works but window pops up as webpage loads as well as when button is pressed

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 
 //generate button and event listener for when clicked//
-document.querySelector('#generate').addEventListener('click', passwordLength);
-  
-// user chooses password length
-function passwordLength() {
-  
-  var length = window.prompt("How many characters would you like your password? Enter a number between 8 and 128.");
-  if (length > 8 || length < 128)  {
-  } else {
-      window.alert("Please enter a length within established parameters.");
-  }
+var button = document.querySelector('#generate');
+button.addEventListener('click', passwordLength());
 
-}
-passwordLength();
+// empty array to hold results of various user input functions
+const password = []
+ 
+
 
 
 // variables in characters
 var specialCharacters = [ '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_'];
-
 
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
@@ -27,7 +20,6 @@ var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M
 
 var variablesAll = ['@', '%', '+', '\\', '/', "'", '!', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -36,73 +28,19 @@ function writePassword() {
   passwordText.value = password;
 }
 
-generatePassword(getUserLength())
-
-function generatePassword(length, lowercase, uppercase, numbers, specialCharacters){
+// user chooses password length
+function passwordLength() {
   
-  // array for lowercase lowercase //
-  const randomLowerCase = lowerCase[Math.floor(Math.random() * lowerCase.length)]
-  
-  
-  // choose if you would like to include lowercase letters?
-   // window prompt for yes or no
-  let userInput = window.prompt("Would you like lowercase letters? Type \"Yes\" or \"No\"")
-  // function to choose random lowercase letter
-function passwordLowercase(){
-  if(userInput === "Yes"); 
-   return randomLowerCase; 
-  }  else {
-   return(???????)
-}
-
-// choose if you would like to include uppercase letters?
-
-// choose if you would like to include numbers?
-
-// choose if you would like to use special characters?
-function random_item()
-{
-  
-return items[Math.floor(Math.random()*items.length)];
-     
-}
-// choose amount of characters to use in password"
-function generatePassword(length, passwordLowercase, passwordUppercase, passwordNumbers, passwordSpecialCharacters){
-
-  // window prompt for character length
-  window.prompt("How long would you like your password? Choose between 8 and 128 characters")
-    function passwordLength() {
-      if (length < 8 || length > 128) 
-        return userInput; // if true, displays users input
-      } else {
-        return
-      
-      }
-    }
-      while (length < 8 || length > 128) {
-      window.prompt("Please choose betwen 8-128 characters");
-
-
-   }
-
-  let totalCount = userCount
-  lowerCount = totalCount - (Random(1, totalCount - 3))
-  upperCount = totalCount - lowerCount - 
-    
-    
-
-//user chooses password length
-//user opts for lowercase
-//getLowercaseChars(length){
-  let result = ""
-  for(let i=0, i < length, i++){
-    result += random
+  var length = prompt("How many characters would you like your password? Enter a number between 8 and 128.");
+  if (length >= 8 || length <= 128)  {
+    console.log(length).upperCase();
+ } else {
+      window.alert("Please enter a length within established parameters.");
   }
 }
-//user opts for uppercase
-//user opts for numbers
-//user opts for specials
-//password is generated from random combination of the above
-//
+passwordLength()
 
-  
+
+// user chooses if they want uppercase letters 
+
+
